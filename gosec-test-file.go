6775441d79999d7b12fb02405190fd136218deb8
akashkumarmg <akash.mg@skyflow.com>
2025-06-06 11:32:10 +0530

    Create gosec-test-file.go

---
+++ gosec-test-file.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func main() {
+	// G204: Subprocess launched with variable
+	userInput := "ls -la"
+	cmd := exec.Command("sh", "-c", userInput)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	fmt.Println(string(output))
+}
