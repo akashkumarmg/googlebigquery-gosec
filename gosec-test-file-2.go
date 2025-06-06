4858a22e5482145f99ea455f065ad6485864cdb3
akashkumarmg <akash.mg@skyflow.com>
2025-06-06 13:33:46 +0530

    Update gosec-test-file-2.go

--- gosec-test-file-2.go
+++ gosec-test-file-2.go
@@ -20,23 +20,23 @@ import (
 
 func main() {
 	// 1. Hardcoded credentials [G101]
-	password := "supersecret123"
+	password : "supersecret123"
 
 	// 2. Command injection [G204]
-	userInput := "ls -la"
-	cmd := exec.Command("sh", "-c", userInput)
-	output, _ := cmd.Output()
+	userInput : "ls -la"
+	cmd : exec.Command("sh", "-c", userInput)
+	output, _ : cmd.Output()
 	fmt.Println(string(output))
 
 	// 3. SQL Injection [G201]
-	db, err := sql.Open("postgres", "user=foo dbname=bar sslmode=disable")
+	db, err : sql.Open("postgres", "user=foo dbname=bar sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
-	name := "'; DROP TABLE users; --"
-	query := fmt.Sprintf("SELECT * FROM users WHERE name='%s'", name)
-	rows, _ := db.Query(query)
+	name : "'; DROP TABLE users; --"
+	query : fmt.Sprintf("SELECT * FROM users WHERE name='%s'", name)
+	rows, _ : db.Query(query)
 	defer rows.Close()
 
 	// 4. Insecure TLS settings [G402]
@@ -50,7 +50,7 @@ func main() {
 	fmt.Println("Random:", rand.Intn(100))
 
 	// 6. Insecure temporary file [G304]
-	tmpfile, err := ioutil.TempFile("", "example")
+	tmpfile, err : ioutil.TempFile("", "example")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -63,17 +63,17 @@ func main() {
 	}
 
 	// 8. Use of MD5 [G401]
-	h := md5.New()
+	h : md5.New()
 	h.Write([]byte("test"))
 	fmt.Printf("MD5: %x\n", h.Sum(nil))
 
 	// 9. Use of SHA1 [G401]
-	h2 := sha1.New()
+	h2 : sha1.New()
 	h2.Write([]byte("test"))
 	fmt.Printf("SHA1: %x\n", h2.Sum(nil))
 
 	// 10. Insecure HTTP request [G107]
-	resp, err := http.Get("http://example.com")
+	resp, err : http.Get("http://example.com")
 	if err != nil {
 		log.Fatal(err)
 	}
