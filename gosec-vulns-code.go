84f48b7c7886dc895a32f4918bc46b22ca2eff16
akashkumarmg <akash.mg@skyflow.com>
2025-07-02 15:52:09 +0530

    Update gosec-vulns-code.go

--- gosec-vulns-code.go
+++ gosec-vulns-code.go
@@ -1,84 +1,143 @@
 package main
 
 import (
-	"crypto/md5"
-	"crypto/sha1"
-	"crypto/tls"
-	"crypto/rand"
-	"database/sql"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"math/rand"
-	"net/http"
-	"os"
-	"os/exec"
-	"time"
+    "database/sql"
+    "fmt"
+    "log"
 
-	_ "github.com/lib/pq"
+    _ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	// 1. Hardcoded credentials [G101]
-	password := "supersecret123"
+    // Hardcoded credentials (Vulnerability 1)
+    dbUser := "admin"
+    dbPassword := "mysecretpassword"
+    dbHost := "localhost"
+    dbName := "mydb"
 
-	// 2. Command injection [G204]
-	userInput := "ls -la"
-	cmd := exec.Command("sh", "-c", userInput)
-	output, _ := cmd.Output()
-	fmt.Println(string(output))
+    // Insecure database connection string (Vulnerability 2)
+    dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s", dbUser, dbPassword, dbHost, dbName)
 
-	// 3. SQL Injection [G201]
-	db, err := sql.Open("postgres", "user=foo dbname=bar sslmode=disable")
+    // Potential SQL injection vulnerability (Vulnerability 3)
+    userInput := "Robert'); DROP TABLE users; --"
+    query := fmt.Sprintf("SELECT * FROM users WHERE name = '%s'", userInput)
+
+    db, err := sql.Open("mysql", dsn)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer db.Close()
+
+    // Insecure logging of sensitive data (Vulnerability 4)
+    log.Printf("Connecting to database with credentials: %s:%s", dbUser, dbPassword)
+
+    // Potential credentials exposure through environment variables (Vulnerability 5)
+    fmt.Println("DB_PASSWORD:", os.Getenv("DB_PASSWORD"))
+
+    // Execute the query
+    rows, err := db.Query(query)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer rows.Close()
+
+    // Print the results
+    for rows.Next() {
+        var name string
+        err := rows.Scan(&name)
+        if err != nil {
+            log.Fatal(err)
+        }
+        fmt.Println(name)
+    }
+}
+
+
+
+package main
+
+import (
+	"crypto/md5"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os/exec"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/dgrijalva/jwt-go"
+)
+
+func main() {
+	// Vulnerability 1: Hardcoded credentials
+	username := "admin"
+	password := "mysecretpassword"
+
+	// Vulnerability 2: Insecure password hashing
+	hash := md5.Sum([]byte(password))
+	fmt.Println("Password hash:", hash)
+
+	// Vulnerability 3: SQL injection
+	db, err := sql.Open("mysql", "user:password@/dbname")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
-	name := "'; DROP TABLE users; --"
-	query := fmt.Sprintf("SELECT * FROM users WHERE name='%s'", name)
-	rows, _ := db.Query(query)
-	defer rows.Close()
 
-	// 4. Insecure TLS settings [G402]
-	_ = &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-		},
-	}
-
-	// 5. Predictable random number generator [G404]
-	fmt.Println("Random:", rand.Intn(100))
-
-	// 6. Insecure temporary file [G304]
-	tmpfile, err := ioutil.TempFile("", "example")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer os.Remove(tmpfile.Name())
-
-	// 7. Insecure file permissions [G302]
-	err = ioutil.WriteFile("secrets.txt", []byte("top-secret"), 0644)
+	userInput := "Robert'); DROP TABLE users; --"
+	query := fmt.Sprintf("SELECT * FROM users WHERE name = '%s'", userInput)
+	_, err = db.Exec(query)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// 8. Use of MD5 [G401]
-	h := md5.New()
-	h.Write([]byte("test"))
-	fmt.Printf("MD5: %x\n", h.Sum(nil))
+	// Vulnerability 4: Command injection
+	cmd := exec.Command("sh", "-c", "echo "+userInput)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(output))
 
-	// 9. Use of SHA1 [G401]
-	h2 := sha1.New()
-	h2.Write([]byte("test"))
-	fmt.Printf("SHA1: %x\n", h2.Sum(nil))
+	// Vulnerability 5: Insecure JWT token signing
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"username": username,
+	})
+	tokenString, err := token.SignedString([]byte("secretkey"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("JWT token:", tokenString)
 
-	// 10. Insecure HTTP request [G107]
+	// Vulnerability 6: Insecure HTTP client
 	resp, err := http.Get("http://example.com")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer resp.Body.Close()
 
-	// 11. Ignoring errors [G103]
-	ioutil.WriteFile("ignored_error.txt", []byte("some data"), 0644)
+	// Vulnerability 7: Missing HTTPS verification
+	httpClient := &http.Client{}
+	httpClient.Transport = &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	}
+
+	// Vulnerability 8: Using vulnerable dependency
+	// github.com/dgrijalva/jwt-go is vulnerable to CVE-2020-26160
+}
+
+func processRequest(w http.ResponseWriter, r *http.Request) {
+	// Vulnerability 9: Potential cross-site scripting (XSS) vulnerability
+	fmt.Fprintf(w, "<script>alert('%s')</script>", r.URL.Query().Get("input"))
+}
+
+func insecureFileHandling() {
+	// Vulnerability 10: Potential path traversal vulnerability
+	filePath := "../sensitive_data.txt"
+	_, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
