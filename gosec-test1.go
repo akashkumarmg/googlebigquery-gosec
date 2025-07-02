4176f6867eab4a7a81d2bc313e876ceedf8596ee
akashkumarmg <akash.mg@skyflow.com>
2025-07-02 15:51:47 +0530

    Update gosec-test1.go

--- gosec-test1.go
+++ gosec-test1.go
@@ -1,84 +1,55 @@
 package main
 
 import (
-	"crypto/md5"
-	"crypto/sha1"
-	"crypto/tls"
-	"crypto/rand"
 	"database/sql"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"math/rand"
 	"net/http"
-	"os"
-	"os/exec"
-	"time"
+	"strings"
 
-	_ "github.com/lib/pq"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	// 1. Hardcoded credentials [G101]
-	password := "supersecret123"
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		username := r.URL.Query().Get("username")
+		password := r.URL.Query().Get("password")
 
-	// 2. Command injection [G204]
-	userInput := "ls -la"
-	cmd := exec.Command("sh", "-c", userInput)
-	output, _ := cmd.Output()
-	fmt.Println(string(output))
+		// SQL Injection vulnerability
+		db, err := sql.Open("sqlite3", "./example.db")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer db.Close()
 
-	// 3. SQL Injection [G201]
-	db, err := sql.Open("postgres", "user=foo dbname=bar sslmode=disable")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-	name := "'; DROP TABLE users; --"
-	query := fmt.Sprintf("SELECT * FROM users WHERE name='%s'", name)
-	rows, _ := db.Query(query)
-	defer rows.Close()
+		var user string
+		err = db.QueryRow("SELECT username FROM users WHERE username = '" + username + "' AND password = '" + password + "'").Scan(&user)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
-	// 4. Insecure TLS settings [G402]
-	_ = &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-		},
-	}
+		// Command Injection vulnerability
+		cmd := fmt.Sprintf("echo 'Hello, %s!'", username)
+		output, err := exec.Command("bash", "-c", cmd).Output()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		fmt.Fprintf(w, "%s", output)
 
-	// 5. Predictable random number generator [G404]
-	fmt.Println("Random:", rand.Intn(100))
+		// Path Traversal vulnerability
+		filePath := r.URL.Query().Get("file")
+		if filePath != "" {
+			http.ServeFile(w, r, "./"+filePath)
+		}
+	})
 
-	// 6. Insecure temporary file [G304]
-	tmpfile, err := ioutil.TempFile("", "example")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer os.Remove(tmpfile.Name())
+	// Cross-Site Scripting (XSS) vulnerability
+	http.HandleFunc("/xss", func(w http.ResponseWriter, r *http.Request) {
+		input := r.URL.Query().Get("input")
+		fmt.Fprintf(w, "<h1>%s</h1>", input)
+	})
 
-	// 7. Insecure file permissions [G302]
-	err = ioutil.WriteFile("secrets.txt", []byte("top-secret"), 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// 8. Use of MD5 [G401]
-	h := md5.New()
-	h.Write([]byte("test"))
-	fmt.Printf("MD5: %x\n", h.Sum(nil))
-
-	// 9. Use of SHA1 [G401]
-	h2 := sha1.New()
-	h2.Write([]byte("test"))
-	fmt.Printf("SHA1: %x\n", h2.Sum(nil))
-
-	// 10. Insecure HTTP request [G107]
-	resp, err := http.Get("http://example.com")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-
-	// 11. Ignoring errors [G103]
-	ioutil.WriteFile("ignored_error.txt", []byte("some data"), 0644)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
