560af9f5b7c12ff07df8ff7bdac6d4b87a0a00c3
akashkumarmg <akash.mg@skyflow.com>
2025-11-18 15:01:41 +0530

    Update and rename gitleaks-test-file.sh to gitleaks-t-file.sh

---
+++ gitleaks-t-file.sh
@@ -0,0 +1,17 @@
+# Fake AWS Access Key
+AWS_ACCESS_KEY_ID=AKIA12345678901234
+
+# Fake AWS Secret Key
+AWS_SECRET_ACCESS_KEY=abcd1234abcd1234abcd1234abcd1234abcd1234
+
+# Fake GitHub Personal Access Token
+GITHUB_TOKEN=ghp_fake1234567890abcdef1234567890abcdef
+
+# Fake Slack Webhook URL
+SLACK_WEBHOOK_URL=https://hooks.slack.com/services/T00000000/B00000000/fake1234567890abcdef
+
+# Fake Private RSA Key
+-----BEGIN RSA PRIVATE KEY-----
+MIIBOgIBAAJBAFAKEKEYTEST1234567890abcdefghijklmnopqrstuvxyzABCDE
+FAKEKEY1234567890abcdefghijklmnopqrstuvxyzABCDEFAKEKEY1234567890
+-----END RSA PRIVATE KEY-----
